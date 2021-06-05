b11d56dcd77481dcb54ebe91edbdc603213c6173
Alireza Razinejad <a.razinejad@gmail.com>
2021-06-05 14:02:20 +0430

    Chapter 3
    - Introduced basic scene
    v0.3.1

---
+++ src/base/basic-scene.base.ts
@@ -0,0 +1,33 @@
+import {ArcRotateCamera, Camera, HemisphericLight, Scene, Vector3} from "@babylonjs/core";
+import {EngineBase} from "./engine.base";
+import {IScene} from "../interfaces/scene.interface";
+
+export class BasicSceneBase extends EngineBase implements IScene {
+    camera: Camera;
+    light: HemisphericLight;
+    scene: Scene;
+
+    constructor() {
+        super()
+        this.scene = new Scene(this.engine)
+
+        this.camera = new ArcRotateCamera(
+            'arc-camera',
+            -Math.PI / 2, Math.PI / 2.5, 5,
+            new Vector3(0, 1, 1),
+            this.scene
+        );
+
+        this.camera.attachControl();
+
+        this.light = new HemisphericLight(
+            'hemi-light',
+            new Vector3(0, 1, 0),
+            this.scene
+        );
+
+        this.engine.runRenderLoop(() => {
+            this.scene.render();
+        });
+    }
+}
