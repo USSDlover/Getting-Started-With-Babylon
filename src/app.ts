e1800c15025d34979a84dc1b15b6b45f2385e77b
Alireza Razinejad <a.razinejad@gmail.com>
2021-06-06 16:43:16 +0430

    Chapter 3
    - Sphere that follow path
    v0.3.8

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,12 @@
 // import {VillageSandbox} from "./getting-started/chapter-two/village.sandbox";
 // import {VillageBabylonGlb} from "./getting-started/chapter-two/village.babylon-glb";
 // import {TheCar} from "./getting-started/chapter-three/the-car";
-import {TheDude} from "./getting-started/chapter-three/the-dude";
+// import {TheDude} from "./getting-started/chapter-three/the-dude";
+import {SphereThatFollowPath} from "./getting-started/chapter-three/sphere-that-follow-path";
 
 class App {
     constructor() {
-        const activeScene = new TheDude();
+        const activeScene = new SphereThatFollowPath();
     }
 }
 
---
+++ src/getting-started/chapter-three/sphere-that-follow-path.ts
@@ -0,0 +1,60 @@
+import {BasicSceneBase} from "../../base/basic-scene.base";
+import {Axis, MeshBuilder, Space, Vector3} from "@babylonjs/core";
+
+export class SphereThatFollowPath extends BasicSceneBase {
+    constructor() {
+        super();
+        this.action();
+    }
+
+    action(): void {
+        const sphere = MeshBuilder.CreateSphere('sphere', {diameter: .25}, this.scene);
+        sphere.position = new Vector3(2,0,2);
+
+        const points = [];
+        points.push(new Vector3(2, 0, 2));
+        points.push(new Vector3(2, 0, -2));
+        points.push(new Vector3(-2, 0, -2));
+        points.push(points[0]);
+
+        MeshBuilder.CreateLines('triangle', {points}, this.scene);
+
+        const slide = function (turn, dist) {
+            this.turn = turn;
+            this.dist = dist;
+        }
+
+        const track = [];
+
+        // First slide length 4
+        track.push(new slide(Math.PI / 2, 4));
+        // At finish of second side distance covered in 4 + 4
+        track.push(new slide(3 * Math.PI / 4, 8));
+        // All three sides cover the distance 4 + 4 + 4 * sqrt(2)
+        track.push(new slide(3 * Math.PI / 4, 8 + 4 * Math.sqrt(2)));
+
+        let distance = 0;
+        let step = .05;
+        let p = 0;
+
+        this.scene.onBeforeRenderObservable.add(() => {
+            sphere.movePOV(0, 0, step);
+            distance += step;
+
+            if (distance > track[p].dist) {
+                sphere.rotate(Axis.Y, track[p].turn, Space.LOCAL);
+                p += 1;
+                p %= track.length;
+                if (p === 0) {
+                    distance = 0;
+                    // Reset to initial conditions
+                    sphere.position = new Vector3(2, 0, 2);
+                    // Prevents error accumulation
+                    sphere.rotation = Vector3.Zero();
+                }
+            }
+        })
+    }
+
+
+}
