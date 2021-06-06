59bc58b45bb050db2ac010954220acf5ccea6ef3
Alireza Razinejad <a.razinejad@gmail.com>
2021-06-06 14:43:26 +0430

    Chapter 3
    - Dude walking around
    v0.3.7

--- src/getting-started/chapter-three/the-dude.ts
+++ src/getting-started/chapter-three/the-dude.ts
@@ -1,8 +1,10 @@
 import {BasicSceneBase} from "../../base/basic-scene.base";
 import '@babylonjs/loaders';
-import {SceneLoader, Vector3} from "@babylonjs/core";
+import {AbstractMesh, Animation, SceneLoader, Vector3} from "@babylonjs/core";
 
 export class TheDude extends BasicSceneBase {
+    dude: AbstractMesh;
+
     constructor() {
         super();
         this.loadTheDude();
@@ -15,11 +17,18 @@ export class TheDude extends BasicSceneBase {
                 'Dude.babylon',
                 this.scene)
             .then((result) => {
-                const dude = result.meshes[0];
-                dude.scaling = new Vector3(.02, .02, .02);
-                dude.position.y = 1;
+                this.dude = result.meshes[0];
+                this.dude.scaling = new Vector3(.02, .02, .02);
+                this.dude.position.y = 1;
 
                 this.scene.beginAnimation(result.skeletons[0], 0, 100, true, 1.0);
+                this.animateDude();
             });
+    }
+
+    animateDude(): void {
+        this.engine.runRenderLoop(() => {
+            this.dude.movePOV(0, 0, 0.01);
+        })
     }
 }
